28cbbb333ed28a182755e6fb3696d81b80fb332b
Paolo Rotolo <rotolopao@gmail.com>
2019-01-25 15:00:40 +0100

    Add Repo

--- app/src/main/java/com/mqtteam/bari2019/activity/MainActivity.kt
+++ app/src/main/java/com/mqtteam/bari2019/activity/MainActivity.kt
@@ -4,16 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mqtteam.bari2019.R
 import com.mqtteam.bari2019.helper.MqttHelper
-import com.mqtteam.bari2019.repository.FirebaseRepository
 
 class MainActivity : AppCompatActivity() {
-    lateinit var mqttHelper: MqttHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        FirebaseRepository.addCarrelloMock()
 
     }
 }
