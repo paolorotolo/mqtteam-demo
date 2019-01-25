806c4d01350b436d14bac1af3e4468e355985c51
Anna Labellarte <anna.labellarte.al@gmail.com>
2019-01-25 15:01:42 +0100

    add layout

---
+++ app/src/main/java/com/mqtteam/bari2019/activity/RecycleViewActivity.kt
@@ -0,0 +1,13 @@
+package com.mqtteam.bari2019.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mqtteam.bari2019.R
+
+class RecycleViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recycle_view)
+    }
+}
